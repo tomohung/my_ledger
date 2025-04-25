d44e147f41bc1dc2c8ac717653b34ee6e2bd200b
Tomo Hung <tomohung@gmail.com>
2025-04-25 22:27:48 +0800

    ci: use standardrb (#4)
    Co-authored-by: standard-ruby-action[bot] <standard-ruby-action[bot]@users.noreply.github.com>

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,12 +1,12 @@
 class PasswordsController < ApplicationController
   allow_unauthenticated_access
-  before_action :set_user_by_token, only: %i[ edit update ]
+  before_action :set_user_by_token, only: %i[edit update]
 
   def new
   end
 
   def create
-    if user = User.find_by(email_address: params[:email_address])
+    if (user = User.find_by(email_address: params[:email_address]))
       PasswordsMailer.reset(user).deliver_later
     end
 
@@ -25,9 +25,10 @@ class PasswordsController < ApplicationController
   end
 
   private
-    def set_user_by_token
-      @user = User.find_by_password_reset_token!(params[:token])
-    rescue ActiveSupport::MessageVerifier::InvalidSignature
-      redirect_to new_password_path, alert: "Password reset link is invalid or has expired."
-    end
+
+  def set_user_by_token
+    @user = User.find_by_password_reset_token!(params[:token])
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to new_password_path, alert: "Password reset link is invalid or has expired."
+  end
 end
