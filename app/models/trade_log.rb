e84fac05695784b496eabd5b58b478b724f2c11a
Tomo Hung <tomohung@gmail.com>
2025-04-28 21:46:13 +0800

    add validation

--- app/models/trade_log.rb
+++ app/models/trade_log.rb
@@ -38,6 +38,8 @@ class TradeLog < ApplicationRecord
   belongs_to :user
   belongs_to :broker_account
 
+  validates :order_id, uniqueness: {scope: :broker_account_id}, allow_nil: true
+
   def self.import_from_csv_string(broker_account, csv_string)
     # Clean up Excel-style formatting after successful parse
     cleaned_csv = csv_string.gsub(/="([^"]*)"/, '\1')
