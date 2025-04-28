d8e9cce78f0153c2ae920eea2bd399662b53c5d6
Tomo Hung <tomohung@gmail.com>
2025-04-28 21:53:36 +0800

    test: trade_log spec (#15)

---
+++ spec/factories/sessions.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: sessions
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  ip_address :string
+#  user_agent :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_sessions_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  user_id  (user_id => users.id)
+#
+FactoryBot.define do
+  factory :session do
+    association :user
+  end
+end
