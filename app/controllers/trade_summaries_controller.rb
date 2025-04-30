6bc8d24c9c526657a5bcd2926a6e1c021e4e930a
Tomo Hung <tomohung@gmail.com>
2025-04-30 19:22:35 +0800

    update

--- app/controllers/trade_summaries_controller.rb
+++ app/controllers/trade_summaries_controller.rb
@@ -35,7 +35,7 @@ class TradeSummariesController < ApplicationController
     month_end = @selected_date.end_of_month
 
     # Get trade statistics
-    trade_stats = current_user.trade_logs
+    @summary = current_user.trade_logs
       .where(trade_date: month_start..month_end)
       .select(
         "COUNT(CASE WHEN net_pnl IS NOT NULL THEN 1 END) as total_trades",
@@ -48,27 +48,6 @@ class TradeSummariesController < ApplicationController
         "COUNT(CASE WHEN net_pnl < 0 THEN 1 END) as losing_trades"
       )
       .first
-
-    # Get or create month report for initial capital
-    month_report = MonthReport.generate_for_month(current_user, month_start)
-
-    # Create a summary object with all the information
-    @summary = OpenStruct.new(
-      total_trades: trade_stats.total_trades,
-      total_gross_pnl: trade_stats.total_gross_pnl,
-      total_commission: trade_stats.total_commission,
-      total_tax: trade_stats.total_tax,
-      total_net_pnl: trade_stats.total_net_pnl,
-      average_net_pnl: trade_stats.average_net_pnl,
-      winning_trades: trade_stats.winning_trades,
-      losing_trades: trade_stats.losing_trades,
-      initial_capital: month_report.initial_capital,
-      remaining_capital: month_report.ending_capital,
-      risk_amount_per_trade: month_report.ending_capital * current_user.risk_amount_percentage_per_trade / 100,
-      risk_amount_per_day: month_report.ending_capital * current_user.risk_amount_percentage_per_day / 100,
-      risk_amount_per_week: month_report.ending_capital * current_user.risk_amount_percentage_per_week / 100,
-      risk_amount_per_month: month_report.ending_capital * current_user.risk_amount_percentage_per_month / 100
-    )
   end
 
   private
