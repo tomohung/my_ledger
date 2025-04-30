cde42eaaae218249b1c50b702403ed49a74769ba
Tomo Hung <tomohung@gmail.com>
2025-04-30 08:33:52 +0800

    feat: reset trade logs (#20)

--- app/controllers/trade_logs_controller.rb
+++ app/controllers/trade_logs_controller.rb
@@ -42,6 +42,12 @@ class TradeLogsController < ApplicationController
     redirect_to trade_logs_path
   end
 
+  def reset
+    current_user.trade_logs.destroy_all
+    flash[:notice] = "所有交易紀錄已重置"
+    redirect_to trade_logs_path
+  end
+
   private
 
   def set_broker_account
