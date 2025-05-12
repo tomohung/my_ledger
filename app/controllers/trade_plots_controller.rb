9570a38f26b7821109dc7ecc30dcf8d74386a212
Tomo Hung <tomohung@gmail.com>
2025-05-12 16:06:52 +0800

    update

--- app/controllers/trade_plots_controller.rb
+++ app/controllers/trade_plots_controller.rb
@@ -9,6 +9,10 @@ class TradePlotsController < ApplicationController
     trades = ParseTradeReport.new(csv_content).call
     @plot_script = PlotTradeRecords.new(trades, merge_to_5min: merge_to_5min).call
 
-    render :new
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.update("plot-script-output", partial: "plot_script", locals: {plot_script: @plot_script})
+      end
+    end
   end
 end
