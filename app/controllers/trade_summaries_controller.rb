af434ae7500c1bb63c8f3ab55207671e6b56d40c
Tomo Hung <tomohung@gmail.com>
2025-05-19 08:35:43 +0800

    update

--- app/controllers/trade_summaries_controller.rb
+++ app/controllers/trade_summaries_controller.rb
@@ -34,7 +34,13 @@ class TradeSummariesController < ApplicationController
       .where(trade_date: month_start..month_end)
       .includes(:broker_account)
 
+    active_trades = @trade_logs.select { |trade| trade.gross_pnl.present? }
+    @buy_trades = active_trades.select { |trade| trade.buy_quantity.positive? }
+    @sell_trades = active_trades.select { |trade| trade.sell_quantity.positive? }
+
     @statistics = AnalyzeTradeLogs.new(@trade_logs).call
+    @buy_statistics = AnalyzeTradeLogs.new(@buy_trades).call
+    @sell_statistics = AnalyzeTradeLogs.new(@sell_trades).call
     @month_report = MonthReport.generate_for_month(current_user, @selected_date.beginning_of_month)
   end
 
